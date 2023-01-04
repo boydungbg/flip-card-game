9f5ebdc3df92c8c7575cfb588efaa09a7d1de731
dunglc <dungle.dev2412@gmail.com>
2023-01-04 21:59:41 +0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+	<main-screen :onChooseSetting="chooseSetting" />
+</template>
+
+<script>
+	import MainScreen from "./Pages/MainScreen.vue";
+	export default {
+		name: "App",
+		components: {
+			"main-screen": MainScreen,
+		},
+		data: () => {
+			return {
+				cards: [],
+			};
+		},
+		methods: {
+			chooseSetting(setting) {
+				this.cards = Array.from(
+					{
+						length: setting / 2,
+					},
+					(_, index) => index + 1
+				);
+				this.cards = [...this.cards, ...this.cards];
+				this.cards.sort(() => {
+          return Math.floor(Math.random()  -0.5);
+        })
+			},
+		},
+	};
+</script>
+
+<style>
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
---
+++ src/Pages/MainScreen.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>POKE MEMORIES</h1>
+  <h3>SELECT MODE TO START</h3>
+  <div>
+    <button @click="chooseSetting(16)">
+      <div>4x4</div>
+      <div>Easy</div>
+    </button>
+    <button @click="chooseSetting(36)">
+      <div>6x6</div>
+      <div>Normal</div>
+    </button>
+    <button @click="chooseSetting(64)">
+      <div>8x8</div>
+      <div>Hard</div>
+    </button>
+    <button @click="chooseSetting(100)">
+      <div>10x10</div>
+      <div>Super Hard</div>
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: "MainScreen",
+  data: () => {
+    return {
+
+    }
+  },
+  methods: {
+    chooseSetting(setting) {
+      this.$emit('chooseSetting',setting );
+    }
+  }
+}
+</script>
+<style>
+
+</style>
